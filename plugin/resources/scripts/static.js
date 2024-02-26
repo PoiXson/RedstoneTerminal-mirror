c4eea5ce23b19dbfb4720f83fa06202f7c6f7fb2
lorenzop <lorenzo@poixson.com>
2024-02-25 19:48:09 -0500

    added static.js script

---
+++ plugin/resources/scripts/static.js
@@ -0,0 +1,31 @@
+/*
+ * Redstone Terminal - Computers in minecraft
+ * copyright 2024
+ * license AGPL-3
+ * lorenzo at poixson.com
+ * https://poixson.com/
+ */
+
+//#fps=12
+//#export=pixels
+
+importClass(Packages.com.poixson.utils.FastNoiseLiteD);
+
+
+
+let count = 0;
+let noise = new FastNoiseLiteD();
+noise.setFrequency(5.0);
+
+
+
+function loop() {
+	count++;
+	let value;
+	for (let iy=0; iy<screen_height; iy++) {
+		for (let ix=0; ix<screen_width; ix++) {
+			value = noise.getNoise(ix, count, iy);
+			pixels[iy][ix] = (value<-0.1 ? Color.WHITE : Color.BLACK);
+		}
+	}
+}
